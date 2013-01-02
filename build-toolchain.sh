1739ea675121149ea8c70cae1a25849f27b1e026
Simon Cook <simon.cook@embecosm.com>
2013-01-02 08:07:56 +0000

    Disable building insight by default.
    Fixes issues with missing X11 headers.

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -101,6 +101,7 @@ mkdir -p ${build_dir} && cd ${build_dir} \
     --enable-fast-install=N/A \
     --enable-languages=c,c++ --prefix=${install_dir} \
     --with-headers=${unified_src}/newlib/libc/include \
+    --disable-gdbtk \
     $CONFIG_EXTRA_OPTS >> "${logfile}" 2>&1
 if [ $? != 0 ]; then
   echo "Error: Configure failed."
